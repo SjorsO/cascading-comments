69d26fec1ea459123f853d6b10e03dc7b3984fbf
Sjors Ottjes <sjorsottjes@gmail.com>
2022-03-03 16:38:56 +0100

    add a header

--- resources/views/components/layout/template/base.blade.php
+++ resources/views/components/layout/template/base.blade.php
@@ -24,6 +24,18 @@
 
 <body>
 
+    <div class="flex items-center justify-between bg-black px-4 py-1 mb-2 text-white">
+        <a class="flex items-center items-end text-sm sm:text-lg" href="/">
+            <img src="{{ asset('favicon.png') }}" width="36" height="36" alt="Cascading Comments logo" class="mr-2">
+            Cascading Comments
+        </a>
+
+        <a class="flex items-center text-sm" href="https://github.com/sjorso/cascading-comments" rel="nofollow">
+            GitHub
+            <x-svg.heroicons.solid.external-link class="w-4 h-4 ml-1"/>
+        </a>
+    </div>
+
     <div class="max-w-4xl w-full mx-auto px-4 py-4">
         {{ $slot }}
     </div>
